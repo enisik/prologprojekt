dab7334053618734fcc4a9bddb2b9685fce86ff5
enisik <51776245+enisik@users.noreply.github.com>
2019-12-23 18:17:34 +0100

    implementation of to_cnf started.

--- sat_solver.pl
+++ sat_solver.pl
@@ -1,7 +1,29 @@
-:- module(sat_solver,[]).
+:- module(sat_solver,[solvername/1,to_cnf/2]).
 % Export the predicates for the test module later on.
 %:- module(sat_solver,[solvername/1, to_cnf/2, solve/1]).
 
 :- load_test_files([]).
 
-% TO DO
+% solvername\1
+solvername(irgendein_name).
+
+% to_cnf/2
+to_cnf(lit(X), [[X]]).
+to_cnf(not(lit(true)), [[false]]):-!.
+to_cnf(not(lit(false)),[[true]]):-!.
+to_cnf(not(lit(X)), [[not(X)]]).
+
+to_cnf(and(Term1, Term2), [Res1, Res2]):-
+    to_cnf(Term1, [Res1]),
+    to_cnf(Term2, [Res2]).
+
+%TODO
+to_cnf(implies(Term1, Term2), [Res]):-
+    .
+
+to_cnf(or(Term1, Term2), [neg(Res1), neg(Res2)]):-
+    to_cnf(not(Term1), [Res1]),
+    to_cnf(not(Term2), [Res2]).
+
+neg([H|T], [ResH|ResT]):-
+    neg()
