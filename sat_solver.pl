42a076510f18d2bf5b2557c3eafd166ef50045d9
enisik <51776245+enisik@users.noreply.github.com>
2020-01-07 12:33:03 +0100

    undo change move to simplify

--- sat_solver.pl
+++ sat_solver.pl
@@ -5,19 +5,45 @@
 :- load_test_files([]).
 
 % solvername\1
-solvername(irgendein_name).
+solvername(prosat).
 
 % to_cnf/2
-
-simplify(not(Term1), not(Term2)):-
-    !,
-    simplify(Term1, Term2).
 
 % a => b  (<=>) -a v b
 simplify(implies(Term1, Term2), or(not(SimpTerm1),SimpTerm2)):-
     !,
     simplify(Term1, SimpTerm1),
     simplify(Term2, SimpTerm2).
+
+
+
+% De Morgan's laws
+simplify(not(or(Term1,Term2)), and(SimpTerm1,SimpTerm2)):-
+    !,
+    simplify(not(Term1), SimpTerm1),
+    simplify(not(Term2), SimpTerm2).
+
+simplify(not(and(Term1,Term2)), or(SimpTerm1,SimpTerm2)):-
+    !,
+    simplify(not(Term1), SimpTerm1),
+    simplify(not(Term2), SimpTerm2).
+
+simplify(not(not(Term1)), SimpTerm1):-
+    !,
+    simplify(Term1, SimpTerm1).
+
+% Distributive property
+simplify(or(Term1, and(Term2,Term3)), and(or(SimpTerm1,SimpTerm2), or(SimpTerm1,SimpTerm3))):-
+    !,
+    simplify(Term1, SimpTerm1),
+    simplify(Term2, SimpTerm2),
+    simplify(Term3, SimpTerm3).
+
+simplify(or(and(Term1,Term2),Term3), and(or(SimpTerm1, SimpTerm3), or(SimpTerm2, SimpTerm3))):-
+    !,
+    simplify(Term1, SimpTerm1),
+    simplify(Term2, SimpTerm2),
+    simplify(Term3, SimpTerm3).
 
 simplify(and(Term1, Term2), and(SimpTerm1, SimpTerm2)):-
     !,
@@ -29,49 +55,11 @@ simplify(or(Term1, Term2), or(SimpTerm1, SimpTerm2)):-
     simplify(Term1, SimpTerm1),
     simplify(Term2, SimpTerm2).
 
+%simplify(not(Term1), not(SimpTerm1)):-
+%    !,
+%    simplify(Term1, SimpTerm1).
+
 simplify(X,X).
-
-move(not(not(Term1)), MovTerm1):-
-    !,
-    move(Term1, MovTerm1).
-
-% De Morgan's laws
-move(not(or(Term1,Term2)), and(MovTerm1,MovTerm2)):-
-    !,
-    move(not(Term1), MovTerm1),
-    move(not(Term2), MovTerm2).
-
-move(not(and(Term1,Term2)), or(MovTerm1,MovTerm2)):-
-    !,
-    move(not(Term1), MovTerm1),
-    move(not(Term2), MovTerm2).
-
-% Distributive property
-move(or(Term1, and(Term2,Term3)), and(or(NewTerm1,NewTerm2), or(NewTerm1,NewTerm3))):-
-    !,
-    move(Term1, NewTerm1),
-    move(Term2, NewTerm2),
-    move(Term3, NewTerm3).
-
-move(or(and(Term1,Term2),Term3), and(or(NewTerm1, NewTerm3), or(NewTerm2, NewTerm3))):-
-    !,
-    move(Term1, NewTerm1),
-    move(Term2, NewTerm2),
-    move(Term3, NewTerm3).
-
-move(and(Term1, Term2), and(SimpTerm1, SimpTerm2)):-
-    !,
-    move(Term1, SimpTerm1),
-    move(Term2, SimpTerm2).
-
-move(or(Term1, Term2), or(SimpTerm1, SimpTerm2)):-
-    !,
-    move(Term1, SimpTerm1),
-    move(Term2, SimpTerm2).
-
-move(X,X).
-
-
 
 to_list(and(Term1, Term2), [Res1, Res2]):-
     !,
@@ -84,7 +72,6 @@ to_list(or(Term1, Term2), [Result]):-
     to_cnf(Term2, [Res2]),
     append(Res1, Res2, Result).
 
-
 to_cnf(lit(X), [[X]]):-!.
 to_cnf(not(lit(X)), [[not(X)]]):-
     \+ ground(X), !.
@@ -94,9 +81,8 @@ to_cnf(not(lit(false)),[[true]]):-!.
 to_cnf(Term, Result):-
     !,
     simplify(Term, SimpTerm),
-    move(SimpTerm, MoveTerm),
-%    distri(MoveTerm, DistriTerm),
-    to_list(MoveTerm, Result).
+    simplify(SimpTerm, SimpTerm2),
+    to_list(SimpTerm2, Result).
 
 % solve/1
 
