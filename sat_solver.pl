11b317941b77e1c60453e3cf799e1edc39f7cf21
enisik <51776245+enisik@users.noreply.github.com>
2020-01-07 21:56:15 +0100

    Version 3.1

--- sat_solver.pl
+++ sat_solver.pl
@@ -74,60 +74,87 @@ simplify_demorgan(Term,Term).
 %simplify_distributive property ---------------------------------------------
 
 % Distributive property
-simplify_distri(or(Term1, and(Term2,Term3)), and(or(SimpTerm1,SimpTerm2), or(SimpTerm1,SimpTerm3))):-
-    !,
-    simplify_distri(Term1, SimpTerm1),
-    simplify_distri(Term2, SimpTerm2),
-    simplify_distri(Term3, SimpTerm3).
+simplify_distri(or(Term1, and(Term2,Term3)), and(or(Term1,Term2), or(Term1,Term3))):- !.
 
-simplify_distri(or(and(Term1,Term2),Term3), and(or(SimpTerm1, SimpTerm3), or(SimpTerm2, SimpTerm3))):-
-    !,
-    simplify_distri(Term1, SimpTerm1),
-    simplify_distri(Term2, SimpTerm2),
-    simplify_distri(Term3, SimpTerm3).
+simplify_distri(or(and(Term1,Term2),Term3), and(or(Term1, Term3), or(Term2, Term3))):-!.
 
-simplify_distri(and(Term1, Term2), and(SimpTerm1, SimpTerm2)):-
-    !,
-    simplify_distri(Term1, SimpTerm1),
+
+simplify_distri(and(Term1, Term2), and(SimpTerm1, Term2)):-
+    simplify_distri(Term1, SimpTerm1).
+
+simplify_distri(and(Term1, Term2), and(Term1, SimpTerm2)):-
     simplify_distri(Term2, SimpTerm2).
 
-simplify_distri(or(Term1, Term2), or(SimpTerm1, SimpTerm2)):-
-    !,
-    simplify_distri(Term1, SimpTerm1),
+simplify_distri(or(Term1, Term2), or(SimpTerm1, Term2)):-
+    simplify_distri(Term1, SimpTerm1).
+
+simplify_distri(or(Term1, Term2), or(Term1, SimpTerm2)):-
     simplify_distri(Term2, SimpTerm2).
 
-simplify_distri(Term,Term).
+%simplify_distri(Term,Term).
 
-iterate(Term1, Term2):-
+distri_loop(Term1, Term2):-
     simplify_distri(Term1, Acc),
     !,
-    iterate(Acc, Term2).
-iterate(Term, Term).
+    distri_loop(Acc, Term2).
+distri_loop(Term, Term).
 
 % to_list converts cnf to list
 to_list(lit(X), [[X]]):-!.
 to_list(not(lit(X)), [[not(X)]]):-
-    \+ ground(X), !.
+    var(X), !.
 to_list(not(lit(true)), [[false]]):-!.
 to_list(not(lit(false)),[[true]]):-!.
 
-to_list(and(Term1, Term2), [Res1|Res2]):-
+to_list(and(Term1, Term2), Result):-
     !,
-    to_cnf(Term1, [Res1]),
-    to_cnf(Term2, Res2).
+    to_list(Term1, Res1),
+    to_list(Term2, Res2),
+    append(Res1,Res2, Result).
 
 to_list(or(Term1, Term2), [Result]):-
     !,
-    to_cnf(Term1, [Res1]),
-    to_cnf(Term2, [Res2]),
+    to_list(Term1, [Res1]),
+    to_list(Term2, [Res2]),
     append(Res1, Res2, Result).
 
 to_cnf(Term, Result):-
     !,
     simplify_implies(Term, SimpImpTerm),
     simplify_demorgan(SimpImpTerm, SimpDeMorganTerm),
-    iterate(SimpDeMorganTerm,Result).
-    %to_list(SimpTerm, Result).
+    distri_loop(SimpDeMorganTerm,SimpTerm),
+    to_list(SimpTerm, Result).
 
 % solve/1
-solve(_):-fail.
+
+remove_value([],[]):-!.
+remove_value([Head|Tail], Result):-
+    member(X, Head),
+    ground(X),
+    member(X, [true]),
+    !,
+    remove_value(Tail,Result).
+remove_value([Head|Tail], [NewHead|TResult]):-
+    member(X, Head),
+    atom(X),
+    delete(Head, X, NewHead),!,
+    remove_value(Tail,TResult).
+remove_value(List, List).
+
+unit_porpagate([],[]).
+unit_porpagate([[Var]|Tail], [[Var]|TResult]):-
+    var(Var),!,
+    Var = true,
+    unit_porpagate(Tail, TResult).
+unit_porpagate([Head|Tail], [Head|TResult]):-
+    unit_porpagate(Tail,TResult).
+
+solve([], 1):-!.
+solve(Term, 1):- member([], Term), !, fail.
+
+solve([]):-!.
+solve(Term):- member([], Term), !, fail.
+solve(Term):-
+    unit_porpagate(Term, Term1),
+    remove_value(Term1, Term2),!,
+    solve(Term2, 1).
