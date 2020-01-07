477f268c1dc536918bfdddc41c3527c55b9c702e
enisik <51776245+enisik@users.noreply.github.com>
2020-01-07 19:32:41 +0100

    Version 3

--- sat_solver.pl
+++ sat_solver.pl
@@ -7,64 +7,114 @@
 % solvername\1
 solvername(prosat).
 
-% to_cnf/2
+%%____ to_cnf/2 ____%%
+
+% simplify_implies ------------------------------------------------------------
 
 % a => b  (<=>) -a v b
-simplify(implies(Term1, Term2), or(not(SimpTerm1),SimpTerm2)):-
+simplify_implies(implies(Term1, Term2), or(not(SimpTerm1),SimpTerm2)):-
     !,
-    simplify(Term1, SimpTerm1),
-    simplify(Term2, SimpTerm2).
+    simplify_implies(Term1, SimpTerm1),
+    simplify_implies(Term2, SimpTerm2).
 
+% simplify inner expression
+simplify_implies(not(Term1), not(SimpTerm1)):-
+    !,
+    simplify_implies(Term1, SimpTerm1).
 
+simplify_implies(and(Term1, Term2),and(SimpTerm1, SimpTerm2)):-
+    !,
+    simplify_implies(Term1, SimpTerm1),
+    simplify_implies(Term2, SimpTerm2).
+
+simplify_implies(or(Term1, Term2),or(SimpTerm1, SimpTerm2)):-
+    !,
+    simplify_implies(Term1, SimpTerm1),
+    simplify_implies(Term2, SimpTerm2).
+
+simplify_implies(Term,Term).
+%-----------------------------------------------------------------------------
+
+% simplify_demorgan and not(not(X)) = X --------------------------------------
 
 % De Morgan's laws
-simplify(not(or(Term1,Term2)), and(SimpTerm1,SimpTerm2)):-
+simplify_demorgan(not(or(Term1,Term2)), and(SimpTerm1,SimpTerm2)):-
     !,
-    simplify(not(Term1), SimpTerm1),
-    simplify(not(Term2), SimpTerm2).
+    simplify_demorgan(not(Term1), SimpTerm1),
+    simplify_demorgan(not(Term2), SimpTerm2).
 
-simplify(not(and(Term1,Term2)), or(SimpTerm1,SimpTerm2)):-
+simplify_demorgan(not(and(Term1,Term2)), or(SimpTerm1,SimpTerm2)):-
     !,
-    simplify(not(Term1), SimpTerm1),
-    simplify(not(Term2), SimpTerm2).
+    simplify_demorgan(not(Term1), SimpTerm1),
+    simplify_demorgan(not(Term2), SimpTerm2).
 
-simplify(not(not(Term1)), SimpTerm1):-
+% not(not(X)) = X
+simplify_demorgan(not(not(Term1)), SimpTerm1):-
     !,
-    simplify(Term1, SimpTerm1).
+    simplify_demorgan(Term1, SimpTerm1).
+
+%simplify inner expression
+simplify_demorgan(not(Term1), not(SimpTerm1)):-
+    !,
+    simplify_demorgan(Term1, SimpTerm1).
+
+simplify_demorgan(and(Term1, Term2),and(SimpTerm1, SimpTerm2)):-
+    !,
+    simplify_demorgan(Term1, SimpTerm1),
+    simplify_demorgan(Term2, SimpTerm2).
+
+simplify_demorgan(or(Term1, Term2),or(SimpTerm1, SimpTerm2)):-
+    !,
+    simplify_demorgan(Term1, SimpTerm1),
+    simplify_demorgan(Term2, SimpTerm2).
+
+simplify_demorgan(Term,Term).
+%------------------------------------------------------------------------------
+
+%simplify_distributive property ---------------------------------------------
 
 % Distributive property
-simplify(or(Term1, and(Term2,Term3)), and(or(SimpTerm1,SimpTerm2), or(SimpTerm1,SimpTerm3))):-
+simplify_distri(or(Term1, and(Term2,Term3)), and(or(SimpTerm1,SimpTerm2), or(SimpTerm1,SimpTerm3))):-
     !,
-    simplify(Term1, SimpTerm1),
-    simplify(Term2, SimpTerm2),
-    simplify(Term3, SimpTerm3).
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2),
+    simplify_distri(Term3, SimpTerm3).
 
-simplify(or(and(Term1,Term2),Term3), and(or(SimpTerm1, SimpTerm3), or(SimpTerm2, SimpTerm3))):-
+simplify_distri(or(and(Term1,Term2),Term3), and(or(SimpTerm1, SimpTerm3), or(SimpTerm2, SimpTerm3))):-
     !,
-    simplify(Term1, SimpTerm1),
-    simplify(Term2, SimpTerm2),
-    simplify(Term3, SimpTerm3).
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2),
+    simplify_distri(Term3, SimpTerm3).
 
-simplify(and(Term1, Term2), and(SimpTerm1, SimpTerm2)):-
+simplify_distri(and(Term1, Term2), and(SimpTerm1, SimpTerm2)):-
     !,
-    simplify(Term1, SimpTerm1),
-    simplify(Term2, SimpTerm2).
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2).
 
-simplify(or(Term1, Term2), or(SimpTerm1, SimpTerm2)):-
+simplify_distri(or(Term1, Term2), or(SimpTerm1, SimpTerm2)):-
     !,
-    simplify(Term1, SimpTerm1),
-    simplify(Term2, SimpTerm2).
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2).
 
-%simplify(not(Term1), not(SimpTerm1)):-
-%    !,
-%    simplify(Term1, SimpTerm1).
+simplify_distri(Term,Term).
 
-simplify(X,X).
+iterate(Term1, Term2):-
+    simplify_distri(Term1, Acc),
+    !,
+    iterate(Acc, Term2).
+iterate(Term, Term).
 
-to_list(and(Term1, Term2), [Res1, Res2]):-
+% to_list converts cnf to list
+to_list(lit(X), [[X]]):-!.
+to_list(not(lit(X)), [[not(X)]]):-
+    \+ ground(X), !.
+to_list(not(lit(true)), [[false]]):-!.
+to_list(not(lit(false)),[[true]]):-!.
+
+to_list(and(Term1, Term2), [Res1|Res2]):-
     !,
     to_cnf(Term1, [Res1]),
-    to_cnf(Term2, [Res2]).
+    to_cnf(Term2, Res2).
 
 to_list(or(Term1, Term2), [Result]):-
     !,
@@ -72,27 +122,12 @@ to_list(or(Term1, Term2), [Result]):-
     to_cnf(Term2, [Res2]),
     append(Res1, Res2, Result).
 
-to_cnf(lit(X), [[X]]):-!.
-to_cnf(not(lit(X)), [[not(X)]]):-
-    \+ ground(X), !.
-to_cnf(not(lit(true)), [[false]]):-!.
-to_cnf(not(lit(false)),[[true]]):-!.
-
 to_cnf(Term, Result):-
     !,
-    simplify(Term, SimpTerm),
-    simplify(SimpTerm, SimpTerm2),
-    to_list(SimpTerm2, Result).
+    simplify_implies(Term, SimpImpTerm),
+    simplify_demorgan(SimpImpTerm, SimpDeMorganTerm),
+    iterate(SimpDeMorganTerm,Result).
+    %to_list(SimpTerm, Result).
 
 % solve/1
-
-solve([]):-!.
-%solve([[]]):-!,fail.
-
-solve([Head|Tail]):-
-    member(true, Head),!,
-    solve(Tail).
-
-solve([Head|Tail]):-
-    member(not(false), Head),
-    solve(Tail).
+solve(_):-fail.
