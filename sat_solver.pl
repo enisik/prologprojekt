939c7cee041e8e10ccc5ec8db55d1d42f6290930
enisik <51776245+enisik@users.noreply.github.com>
2020-01-09 21:19:06 +0100

    to_cnf broken again:/

--- sat_solver.pl
+++ sat_solver.pl
@@ -74,29 +74,35 @@ simplify_demorgan(Term,Term).
 %simplify_distributive property ---------------------------------------------
 
 % Distributive property
-simplify_distri(or(Term1, and(Term2,Term3)), and(or(Term1,Term2), or(Term1,Term3))):- !.
-
-simplify_distri(or(and(Term1,Term2),Term3), and(or(Term1, Term3), or(Term2, Term3))):-!.
-
-
-simplify_distri(and(Term1, Term2), and(SimpTerm1, Term2)):-
-    simplify_distri(Term1, SimpTerm1).
-
-simplify_distri(and(Term1, Term2), and(Term1, SimpTerm2)):-
-    simplify_distri(Term2, SimpTerm2).
-
-simplify_distri(or(Term1, Term2), or(SimpTerm1, Term2)):-
-    simplify_distri(Term1, SimpTerm1).
-
-simplify_distri(or(Term1, Term2), or(Term1, SimpTerm2)):-
-    simplify_distri(Term2, SimpTerm2).
-
-
-distri_loop(Term1, Result):-
-    simplify_distri(Term1, Term2),
+simplify_distri(or(Term1, and(Term2,Term3)), and(or(SimpTerm1,SimpTerm2), or(SimpTerm1,SimpTerm3))):-
     !,
-    distri_loop(Term2, Result).
-distri_loop(Term, Term).
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2),
+    simplify_distri(Term3, SimpTerm3).
+
+simplify_distri(or(and(Term1,Term2),Term3), and(or(SimpTerm1, SimpTerm3), or(SimpTerm2, SimpTerm3))):-
+    !,
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2),
+    simplify_distri(Term3, SimpTerm3).
+
+simplify_distri(and(Term1, Term2), and(SimpTerm1, SimpTerm2)):-
+    !,
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2).
+
+simplify_distri(or(Term1, Term2), or(SimpTerm1, SimpTerm2)):-
+    !,
+    simplify_distri(Term1, SimpTerm1),
+    simplify_distri(Term2, SimpTerm2).
+
+simplify_distri(Term,Term).
+
+%distri_loop(Term1, Result):-
+%    simplify_distri(Term1, Term2),
+%    !,
+%    distri_loop(Term2, Result).
+%distri_loop(Term, Term).
 
 % to_list converts cnf to list
 to_list(lit(X), [[X]]):-!.
@@ -121,18 +127,19 @@ to_cnf(Term, Result):-
     !,
     simplify_implies(Term, SimpImpTerm),
     simplify_demorgan(SimpImpTerm, SimpDeMorganTerm),
-    distri_loop(SimpDeMorganTerm,SimpTerm),
+    simplify_distri(SimpDeMorganTerm,SimpTerm),
     to_list(SimpTerm, Result).
 
 % solve/1
 
 remove_false([],[]):-!.
 remove_false([Head|Tail], Result):-
-    ground(Head),!,
-    (Head == false ; Head == not(true)), % <-- unsat3 schlägt fehl, wenn hier ein cut ist, statt oben
+    ground(Head),
+    (Head == false ; Head == not(true)),!,
     remove_false(Tail,Result).
 remove_false([Head|Tail], [Head|TResult]):-
     remove_false(Tail,TResult).
+
 
 remove_value([],[]):-!.
 remove_value([Head|Tail], Result):-
@@ -146,45 +153,53 @@ remove_value([Head|Tail], [NewHead|TResult]):-
     remove_value(Tail,TResult).
 remove_value(List, List).
 
+
 unit_propagate([],[]):-!.
 unit_propagate([[Var]|Tail], Tail):-
     var(Var),
     Var = true.
-unit_propagate([[not(Var)|Tail]], Tail):-
+unit_propagate([[not(Var)]|Tail], Tail):-
     var(Var),
     Var = false.
 unit_propagate([Head|Tail], [Head|TResult]):-
     unit_propagate(Tail,TResult).
 
 unit_prop_and_remove(List,Result):-
-    remove_value(List, CleanedList),
-    unit_propagate(CleanedList, NewList),
+    %remove_value(List, CleanedList),
+    has_unit(List),!,
+    unit_propagate(List, NewList),
     remove_value(NewList,Result).
+
+has_unit(Term):-
+    member([_Var], Term).
 
 propagate([Head|Tail], Result):-
     member(X, Head),
     var(X),
-    member(X, [true,false]),
+    (X = true; X = false),
     remove_value([Head|Tail], Result).
 
 propagate([Head|Tail], Result):-
     member(not(X), Head),
-    var(X),
-    member(X, [true,false]),
+    var(X),!,
+    (X = true; X = false),
+    %member(X, [true,false]),
     remove_value([Head|Tail], Result).
 
-solve_helper([]):-!.
-solve_helper(Term):- member([], Term), !, fail.
 
-solve_helper(Term):-
-    unit_prop_and_remove(Term, NewTerm),
-    solve_helper(NewTerm).
+dpll([]):-!.
+dpll(Term):- member([], Term), !, fail.
 
-solve_helper(Term):-
+dpll(Term):-
+    unit_prop_and_remove(Term, NewTerm),!,
+    dpll(NewTerm).
+
+dpll(Term):-
     propagate(Term, NewTerm),
-    solve_helper(NewTerm).
+    dpll(NewTerm).
 
 
 solve(Term):-
-    unit_prop_and_remove(Term, NewTerm),!,
-    solve_helper(NewTerm).
+    %nit_prop_and_remove(Term, NewTerm),!,
+    remove_value(Term, NewTerm),!,
+    dpll(NewTerm).
